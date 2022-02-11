de8d95edd850047a61f3d9d028ed6eccce6921aa
blautista <bautistaromanazzi@gmail.com>
2022-02-11 15:24:19 -0300

    refactored code

--- randomMovement/app.js
+++ randomMovement/app.js
@@ -1,30 +1,37 @@
-const container = document.getElementById("container");
+const containerId = "container";
 
-container.addEventListener("hover", () => {
-  console.log("hoverd");
-});
+const getCSSVariable = (name) =>
+  getComputedStyle(document.documentElement).getPropertyValue(name);
+
+const setCSSVariable = (name, value) => {
+  document.querySelector(":root").style.setProperty(name, value);
+};
+
+const restartAnimation = async (container, animationClassName) => {
+  container.classList.remove(animationClassName);
+  await new Promise((d) => setTimeout(d, 50));
+  container.classList.add(animationClassName);
+};
+
+const rand = (from = 0, to = 1, intOnly = true) => {
+  const newRand = Math.random() * to + from;
+  if (intOnly) return Math.floor(newRand);
+  else return newRand;
+};
 
 document.addEventListener("animationend", async () => {
+  const container = document.getElementById(containerId);
+
   const containerRect = container.getBoundingClientRect();
   const containerX = containerRect.width;
   const containerY = containerRect.height;
-  let newYPos = Math.floor(Math.random() * (window.innerHeight - containerY));
-  let newXPos = Math.floor(Math.random() * (window.innerWidth - containerX));
+  let newYPos = rand(0, window.innerHeight - containerY);
+  let newXPos = rand(0, window.innerWidth - containerX);
 
-  container.classList.remove("animate");
-  container.style.top = getComputedStyle(
-    document.documentElement
-  ).getPropertyValue("--final-top");
-  container.style.left = getComputedStyle(
-    document.documentElement
-  ).getPropertyValue("--final-left");
+  container.style.top = getCSSVariable("--final-top");
+  container.style.left = getCSSVariable("--final-left");
+  setCSSVariable("--final-top", `${newYPos}px`);
+  setCSSVariable("--final-left", `${newXPos}px`);
 
-  document
-    .querySelector(":root")
-    .style.setProperty("--final-top", `${newYPos}px`);
-  document
-    .querySelector(":root")
-    .style.setProperty("--final-left", `${newXPos}px`);
-  await new Promise((d) => setTimeout(d, 1000));
-  container.classList.add("animate");
+  await restartAnimation(container, "animate");
 });
